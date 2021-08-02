5d739445d485a85e2e1af7084cd6d3a7206608f7
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 09:46:26 -0500

    used fs instead of throwing shit around

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ prefix: ["F!"]
 
 bot.MessageEvent()
 
+const fs = require('fs');
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+    const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+    for (const commands of folder) {
+        const command = require(`./commands/${files}/${commands}`)
+        bot.Command(command);
+        console.log(`Loaded: ${command.name} | ${files} `);
+    }
+}
+
 bot.Variables({
   cash:"0",
   bank:"0"
