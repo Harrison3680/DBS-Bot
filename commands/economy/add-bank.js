f6b57c46d443f7fb2c16c2c0cf16002e0d1cfb6b
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:57:25 -0500

    Update add-bank.js

--- commands/economy/add-bank.js
+++ commands/economy/add-bank.js
@@ -1,1 +1,9 @@
 
+ module.exports = ({
+    name:"add-bank",
+    code:`$title[Cash added to bank]
+    $description[Successfully added $$message[1] to $username[$mentioned[1]]'s bank]
+    $setUserVar[bank;$sum[$getUserVar[bank;$mentioned[1]];$message[1]];$mentioned[1]]
+    $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;You need to be Speshul, Chris, or Ricky to use this command]`
+    })
+    
