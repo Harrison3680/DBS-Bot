18d9d62f0a3d0db4c5282c7169f741452f7d7942
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:06:04 -0500

    Update hack.js

--- commands/economy/hack.js
+++ commands/economy/hack.js
@@ -1,1 +1,8 @@
-
+module.exports = ({
+    name:"hack",
+    code:`$setUserVar[bank;$sum[$getUserVar[bank;$authorID];$random[500;1000]];$authorID]
+$title[hacking for DB-Script]
+$description[You hacked $randomText[Amazon;Google;NPM;Github;Ebay;Freenom;Gitbook;Ricky's Bank Acc] and earned $$random[500;1000] from speshul]
+$color[RANDOM]
+$globalCooldown[2h;you may be caught by the FBI if you hack in a short amount of time. Come back in {time} for another hacking job.]`
+})
