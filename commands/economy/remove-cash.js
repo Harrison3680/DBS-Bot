eeadfc9525d8984ad652e2c4660a371465757a0d
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:07:59 -0500

    Update remove-cash.js

--- commands/economy/remove-cash.js
+++ commands/economy/remove-cash.js
@@ -1,1 +1,8 @@
+       module.exports = ({
+        name:"remove-cash",
+        code:`$title[Cash removed]
+        $description[Successfully removed $$message[1] from $username[$mentioned[1]]]
+        $setUserVar[cash;$sub[$getUserVar[cash;$mentioned[1]];$message[1]];$mentioned[1]]
+        $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;You need to be Speshul, Chris, or Ricky to use this command]`
+        })
 
