d38a5286f13617e935c3ee8a537117b49def21ce
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:12:44 -0500

    added

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -1,1 +1,9 @@
-
+module.exports = ({
+            name:"withdraw",
+            code:`$description[you have deposited $$message[1] into your bank]
+            $setUserVar[cash;$sum[$getUserVar[cash;$authorID];$message[1]];$authorID]
+            $setUserVar[bank;$sub[$getUserVar[bank;$authorID];$message[1]];$authorID]
+            $onlyIf[$message[1]<1;You cannot withdraw or deposit less than 1.]
+            $onlyIf[$message[1]>$getUserVar[bank;$authorID];You cannot withdraw more than you have]
+            $onlyForUsers`
+        })
