037ce660fd87bb2fcc548d307e6cacd372d9f5e6
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:09:41 -0500

    added

--- commands/utiilities/help.js
+++ commands/utiilities/help.js
@@ -1,1 +1,15 @@
+module.exports = ({
+    name:"help",
+    code:`Work 
+    hack
+    crime
+    daily
+    weekly
+    balance
+    add-bank
+    add-cash
+    remove-bank
+    remove-cash
+    eval`
+})
 
