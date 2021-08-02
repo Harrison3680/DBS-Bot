a480eba0e885cc4373d48634e17b35a481ede967
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:10:39 -0500

    added

--- commands/utiilities/eval.js
+++ commands/utiilities/eval.js
@@ -1,1 +1,5 @@
-
+module.exports = ({
+          name:"eval",
+          code:`$eval[$message[]]
+          $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;634860829132455937;NO]
+          `
