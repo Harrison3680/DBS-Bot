fea370c252f0f31d6061ab895757efff88f9abca
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:58:59 -0500

    Update add-cash.js

--- commands/economy/add-cash.js
+++ commands/economy/add-cash.js
@@ -1,1 +1,7 @@
-
+module.exports = ({
+    name:"add-cash",
+    code:`$title[Cash added]
+    $description[Successfully added $$message[1] to $username[$mentioned[1]]]
+    $setUserVar[cash;$sum[$getUserVar[cash;$mentioned[1]];$message[1]];$mentioned[1]]
+    $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;You need to be Speshul, Chris, or Ricky to use this command]`
+    })
