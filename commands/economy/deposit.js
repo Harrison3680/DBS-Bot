310bc7e5ce51f6de931f0aae2f7be8a0576da341
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:03:39 -0500

    Update deposit.js

--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -1,1 +1,8 @@
-
+module.exports = ({
+            name:"deposit",
+            code:`$description[you have deposited $$message[1] into your bank]
+            $setUserVar[bank;$sum[$getUserVar[bank;$authorID];$message[1]];$authorID]
+            $setUserVar[cash;$sub[$getUserVar[cash;$authorID];$message[1]];$authorID]
+            $onlyIf[$message[1]<1;You cannot withdraw or deposit less than 1.]
+            $onlyIf[$message[1]>$getUserVar[cash;$authorID];You cannot deposit more than you have]`
+        })
