4c9b7fb22f9ad962a50e95a677294c94748e804c
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 12:53:10 -0500

    Update djsEval.js

--- commands/utiilities/djsEval.js
+++ commands/utiilities/djsEval.js
@@ -1,1 +1,6 @@
-
+module.exports = ({
+          name:"deval",
+          code:`$djsEval[$message[]]
+          $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;634860829132455937;NO]
+          `
+})
