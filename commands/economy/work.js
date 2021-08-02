4aa20468561c17ed67ba833b2add4a92b4941274
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:11:37 -0500

    added

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -1,1 +1,8 @@
-
+module.exports = ({
+    name:"work",
+    code:`$setUserVar[cash;$sum[$getUserVar[cash;$authorID];$random[100;500]];$authorID]
+$title[Working for DB-Script]
+$description[You worked as $randomText[staff.TrialSupport();staff.Support();staff.Mod();staff.Admin();staff.Lead();client.Developer()] and earned $$random[100;500] from speshul]
+$color[RANDOM]
+$globalCooldown[2h;take a break, You can work again in {time}]`
+})
