81717964e47d6aba1de2fafb4b199865e0ab1bb7
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:01:42 -0500

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,1 +1,8 @@
-
+module.exports = ({
+	name: "daily",
+	code: `$setVar[cash;$sum[$getVar[cash;$authorID];$random[500;650]];$authorID]
+$title[DAILY]
+$description[You claimed your daily from ricky and got $$random[500;650]]
+$color[RANDOM]
+$globalCooldown[24h;You can claim again in {time}]`
+})
