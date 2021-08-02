0fad3efd10594a08ffe11008dc7c7db2543caa20
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 10:59:44 -0500

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,1 +1,11 @@
-
+module.exports = ({
+    name:"balance",    
+    code: `
+    $author[$username[$mentioned[1;yes]]'s Balance]
+    $color[RANDOM]
+    $addField[🏦Bank Acc :;$$getUserVar[bank;$mentioned[1;yes]]]
+    $addField[💰Wallet :;$$getUserVar[cash;$mentioned[1;yes]]]
+    $addField[Networth:;$$abbreviate[$sum[$getUserVar[bank;$mentioned[1;yes]];$getUserVar[cash;$mentioned[1;yes]]]]]
+    $onlyIf[$isBot[$mentioned[1]]!=true;Bots don't have money!]`
+    })
+    
