33787da5acb3e5607136f5cf879aef4deab87d60
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:00:36 -0500

    Update crime.js

--- commands/economy/crime.js
+++ commands/economy/crime.js
@@ -1,1 +1,9 @@
+module.exports = ({
+    name:"crime",
+    code:`$setUserVar[bank;$sum[$getUserVar[bank;$authorID];$random[1500;2000]];$authorID]
+$title[Crime]
+$description[You commited $randomText[Murder;Bank Robbery;Store Robbery;Credit Card Fraud;Drug Dealing;a Hitman Job;Income Tax Evasion] and earned $$random[500;1000] from ricky]
+$color[RANDOM]
+$globalCooldown[4h;Wait before you can commit another crime. Come back in {time} for another crime job.]`
+})
 
