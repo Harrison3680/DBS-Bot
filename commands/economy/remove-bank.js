f9ca8f57a8c50542d4285f0751be2a1f2915ff98
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 11:07:00 -0500

    Update remove-bank.js

--- commands/economy/remove-bank.js
+++ commands/economy/remove-bank.js
@@ -1,1 +1,8 @@
-
+ module.exports = ({
+        name:"remove-bank",
+        code:`$title[Cash removed from bank]
+        $description[Successfully removed $$message[1] from $username[$mentioned[1]]'s bank']
+        $setUserVar[bank;$sub[$getUserVar[bank;$mentioned[1]];$message[1]];$mentioned[1]]
+        $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;You need to be Speshul, Chris, or Ricky to use this command]`
+        })
+        
