e9d4b6f1942fe4a7455a00882eaf7c19e0b5374e
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 20:37:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,6 @@ token: config.TOKEN,
 prefix: ["F!"]
 })
 
-
-client.on('ready', () => {
-
-setInterval(() => {
-        exec(`git pull`, (error, stdout) => {
-            let response = (error || stdout);
-            if (!error) {
-                if (response.includes("Already up to date.")) {
-                    console.log('Bot already up to date. No changes since last pull')
-                } else {
-                    client.channels.cache.get('871915599788380230').send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
-                    setTimeout(() => {
-                        process.exit();
-                    }, 1000)
-                }
-            }
-        })
-    }, 15000)
-})
-
 bot.MessageEvent()
 
 const fs = require('fs');
