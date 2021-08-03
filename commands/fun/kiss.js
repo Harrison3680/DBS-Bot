54678fd624b611e62b812374563e79ddf31450aa
Ricky <47152942+RickyDana06@users.noreply.github.com>
2021-08-03 11:09:08 -0400

    Update kiss.js

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -1,5 +1,6 @@
 module.exports = ({
-  name:`kiss`,
-  code:`$title[$username[$authorID] kisses $username[$mentioned[1]]]
-  $image[$randomText[https://media.giphy.com/media/l2Je2M4Nfrit0L7sQ/giphy.gif;https://media.giphy.com/media/eetr3eYVSe7xm/giphy.gif;https://media.giphy.com/media/l41YjOzjypzrIxInC/giphy.gif;https://media.giphy.com/media/vB6GQ7Ogc6j4I/giphy.gif;https://media.giphy.com/media/Ij1cbMbIWDKDK/giphy.gif;https://media.giphy.com/media/3o72F3zlbWvP4kJp4c/giphy.gif; https://media.giphy.com/media/wDEWXcisSjrQQ/giphy.gif;https://media.giphy.com/media/g2qW795KmuqSk/giphy.gif;https://media.giphy.com/media/YgKGVzHaEb1As/giphy.gif; https://media.giphy.com/media/3o7TKEon7mFwdygLYY/giphy.gif;https://media.giphy.com/media/9XA2IYCfuPyHC/giphy.gif;https://media.giphy.com/media/3o6EhXtR57VVizHpGo/giphy.gif;https://media.giphy.com/media/l2SpQaZIPgpdwBTI4/giphy.gif]]`
-  })
+  name: `kiss`,
+  code: `
+  $title[$username[$authorID] kisses $username[$findUser[$message[1]]]]
+  $image[$randomText[https://media.giphy.com/media/l2Je2M4Nfrit0L7sQ/giphy.gif;https://media.giphy.com/media/eetr3eYVSe7xm/giphy.gif;https://media.giphy.com/media/l41YjOzjypzrIxInC/giphy.gif;https://media.giphy.com/media/vB6GQ7Ogc6j4I/giphy.gif;https://media.giphy.com/media/Ij1cbMbIWDKDK/giphy.gif;https://media.giphy.com/media/3o72F3zlbWvP4kJp4c/giphy.gif; https://media.giphy.com/media/wDEWXcisSjrQQ/giphy.gif;https://media.giphy.com/media/g2qW795KmuqSk/giphy.gif;https://media.giphy.com/media/YgKGVzHaEb1As/giphy.gif; https://media.giphy.com/media/3o7TKEon7mFwdygLYY/giphy.gif;https://media.giphy.com/media/9XA2IYCfuPyHC/giphy.gif;https://media.giphy.com/media/3o6EhXtR57VVizHpGo/giphy.gif;https://media.giphy.com/media/l2SpQaZIPgpdwBTI4/giphy.gif]]
+  `})
