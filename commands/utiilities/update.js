dccda9bb9098d3037252a4a713921f3c42bb4b09
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-03 17:20:14 -0500

    Update update.js

--- commands/utiilities/update.js
+++ commands/utiilities/update.js
@@ -1,23 +1,3 @@
 module.exports = {
   name:"git-pull",
-  code:`$djsEval[const exec = require('child_process').exec;
-    if (message.member.roles.cache.find(r => r.id === "831412883270926387")) {
-        exec(`git pull`, (error, stdout) => {
-            let response = (error || stdout);
-            if (!error) {
-                if (response.includes("Already up to date.")) {
-                    message.channel.send('Bot already up to date. No changes since last pull')
-                } else {
-                    message.channel.send('Pulled from GitHub. Restarting bot. \n\nLogs: \n```' + response + "```")
-                    setTimeout(() => {
-                        process.exit();
-                    }, 1000)
-                }
-                ;
-            }
-        });
-    } else {
-        message.channel.send('OwO')
-    }
-}]`
-  }
+  code:`$exec[git pull origin main]`
