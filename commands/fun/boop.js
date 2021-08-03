85b9adc9dbe632b3f0389bea8eae8c36a24fe229
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 22:19:38 -0500

    Update boop.js

--- commands/fun/boop.js
+++ commands/fun/boop.js
@@ -1,5 +1,5 @@
 module.exports = ({
 name:`boop`,
 code:`$title[$username[$authorID] boops $username[$mentioned[1]]]
-$image[$randomText[https://media.giphy.com/media/aCqb9YW7QclN3rtto5/giphy.gif;https://media.giphy.com/media/99LhY1qc6jG8w/giphy.gif;https://media.giphy.com/media/BUlwrDd97bCYE/giphy.gif;https://media.giphy.com/media/3oKIPe05SZ9F2reImY/giphy.gif;https://media.giphy.com/media/oeYpYm0BOEDWo/giphy.gif;https://media.giphy.com/media/x4bWE4pS66NEPZR9G1/giphy.gif;https://media.giphy.com/media/SYLvjCEtBClsS2QePl/giphy.gif;https://media.giphy.com/media/hSvBxxFsk0jhm/giphy.gif;https://media.giphy.com/media/lcvjDNIJ8CS88/giphy.gif;https://media.giphy.com/media/naAaDvbAoOYdW/giphy.gif;https://media.giphy.com/media/TyqySo3eDFLS8/giphy.gif]]
+$image[$randomText[https://media.giphy.com/media/aCqb9YW7QclN3rtto5/giphy.gif;https://media.giphy.com/media/99LhY1qc6jG8w/giphy.gif;https://media.giphy.com/media/BUlwrDd97bCYE/giphy.gif;https://media.giphy.com/media/3oKIPe05SZ9F2reImY/giphy.gif;https://media.giphy.com/media/oeYpYm0BOEDWo/giphy.gif;https://media.giphy.com/media/x4bWE4pS66NEPZR9G1/giphy.gif;https://media.giphy.com/media/SYLvjCEtBClsS2QePl/giphy.gif;https://media.giphy.com/media/hSvBxxFsk0jhm/giphy.gif;https://media.giphy.com/media/lcvjDNIJ8CS88/giphy.gif;https://media.giphy.com/media/naAaDvbAoOYdW/giphy.gif;https://media.giphy.com/media/TyqySo3eDFLS8/giphy.gif]]`
 })
