069545062f7bf8642bfc5decaf7a74c3bd1fc00e
Ricky <47152942+RickyDana06@users.noreply.github.com>
2021-08-03 11:00:14 -0400

    Update slap.js

--- commands/fun/slap.js
+++ commands/fun/slap.js
@@ -1,5 +1,6 @@
 module.exports = ({
 name:"slap",
-code:`$title[$username[$authorID] slaps $username[$findUser[$message[1]]]]
+code: `
+$title[$username[$authorID] slaps $username[$findUser[$message[1]]]]
 $image[$randomText[https://media.giphy.com/media/jt38YxwGTevEkFWWoY/giphy.gif;https://media.giphy.com/media/3ohfFOrOAW9GaczHc4/giphy.gif;https://media.giphy.com/media/3XlEk2RxPS1m8/giphy.gif;https://media.giphy.com/media/uqSU9IEYEKAbS/giphy.gif;https://media.giphy.com/media/u8maN0dMhVWPS/giphy.gif;https://media.giphy.com/media/Qvwc79OfQOa4g/giphy.gif]]
-})
+`})
