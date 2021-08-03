c1ddfaeb89f9e73a79481fa86c89a5f2bf1634b2
Ricky <47152942+RickyDana06@users.noreply.github.com>
2021-08-03 11:01:26 -0400

    Update speedforce.js

--- commands/fun/speedforce.js
+++ commands/fun/speedforce.js
@@ -1,5 +1,6 @@
 module.exports = ({
-name:`speedforce`,
-code:`$title[$username[$authorID] has run into the speedforce]
-$image[$randomText[https://cdn.discordapp.com/attachments/734047444895268942/842467322768654356/tumblr_inline_o72hlippdT1ss1lam_500.gif;https://cdn.discordapp.com/attachments/734047444895268942/842509804295684097/barry-speedforce.gif;https://cdn.discordapp.com/attachments/734047444895268942/842509221362663474/barry.gif]]`
-})
+name: `speedforce`,
+code: `
+$title[$username[$authorID] has run into the speedforce]
+$image[$randomText[https://cdn.discordapp.com/attachments/734047444895268942/842467322768654356/tumblr_inline_o72hlippdT1ss1lam_500.gif;https://cdn.discordapp.com/attachments/734047444895268942/842509804295684097/barry-speedforce.gif;https://cdn.discordapp.com/attachments/734047444895268942/842509221362663474/barry.gif]]
+`})
