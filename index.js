c1a25a5efb3e3f51ec105da5290822685bf9da11
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-02 19:41:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exec = require('child_process').exec;
 global.config = require("./config.json")
 const Dlang = require('discordbot-script')
 const { processes } = require('systeminformation')
@@ -5,6 +6,22 @@ const bot = new Dlang({
 token: config.TOKEN,
 prefix: ["F!"]
 })
+
+  setInterval(() => {
+        exec(`git pull`, (error, stdout) => {
+            let response = (error || stdout);
+            if (!error) {
+                if (response.includes("Already up to date.")) {
+                    //console.log('Bot already up to date. No changes since last pull')
+                } else {
+                    client.channels.cache.get('871915599788380230').send('**[AUTOMATIC]** \nNew update on GitHub. Pulling. \n\nLogs: \n```' + response + "```" + "\n\n\n**Restarting bot**")
+                    setTimeout(() => {
+                        process.exit();
+                    }, 1000)
+                }
+            }
+        })
+    }, 60000)
 
 bot.MessageEvent()
 
