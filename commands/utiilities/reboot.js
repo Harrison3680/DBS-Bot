21ed443e54532652d0dd0428ec77105020637f67
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 07:51:39 -0500

    Update reboot.js

--- commands/utiilities/reboot.js
+++ commands/utiilities/reboot.js
@@ -1,5 +1,5 @@
-module.exports = ({
+module.exports = {
 name:"reboot",
 code:`$reboot[Rebooting bot...]
 $onlyForUsers[600824086943367168;376120479498633238;672215920558604332;634860829132455937;NO]`
-})
+}
