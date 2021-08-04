a3ee1980acd95734775763f5c71a50f66eaeabdd
Harrison3680 <76897015+Harrison3680@users.noreply.github.com>
2021-08-04 03:59:55 -0500

    Update ready.js

--- commands/utiilities/ready.js
+++ commands/utiilities/ready.js
@@ -1,5 +1,5 @@
 module.exports.ReadyCommand = ({
-name: " ",
+name: " 858387930703724564",
 code: `
 $client[tag] has been initiated and connected to the discord api at $client[readyat] using DB-Scripr Version :$packageVersion. 
 `
